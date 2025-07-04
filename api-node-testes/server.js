ecacc6a8b97b3d6783596d1c3cf3952f636b5ee6
gabrie277 <gabrieloliveiraramos38@gmail.com>
2025-07-04 15:52:04 -0300

    novo server

--- api-node-testes/server.js
+++ api-node-testes/server.js
@@ -1,34 +1,16 @@
-// Importa o framework Express para facilitar a criação do servidor HTTP
 const express = require("express");
-
-// Importa o pacote CORS para permitir requisições de outras origens
+const bodyParser = require("body-parser");
 const cors = require("cors");
+const livroRoutes = require("./routes/livroRoutes");//const livroRoutes = require("./routes/livroRoutes");
 
-// Importa o pacote para interpretar XML no corpo das requisições
-const xmlparser = require("express-xml-bodyparser");
-
-// Cria uma instância da aplicação Express
 const app = express();
-
-// Ativa o middleware CORS para todas as rotas
-app.use(cors()); // <-- Permite requisições de outras origens
-
-// Define a porta em que o servidor vai rodar
 const PORT = 3000;
 
-// Adiciona um middleware para permitir que a aplicação interprete JSON no corpo das requisições
-app.use(express.json());
-// Adiciona um middleware para permitir que a aplicação interprete XML no corpo das requisições
-app.use(xmlparser());
+app.use(cors());
+app.use(bodyParser.json());
 
-// Importação das rotas dos usuários
-const usuarioRoutes = require("./routes/usuarioRoutes");
+app.use("/livros", livroRoutes);
 
-// Usando as rotas dos usuários
-app.use("/", usuarioRoutes);
-
-// Inicia o servidor na porta especificada e exibe uma mensagem no console
 app.listen(PORT, () => {
-  console.log(`Servidor funcionando`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
-
