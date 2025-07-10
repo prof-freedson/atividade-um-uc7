e945e5737e8798e12fa15e3ef2074ffd5c479c6b
mgrda <deyservieira@gmail.com>
2025-07-10 16:26:51 -0300

    atualização da usuario model

--- api-node-testes/models/usuarioModel.js
+++ api-node-testes/models/usuarioModel.js
@@ -2,17 +2,17 @@ const fs = require("fs");
 const path = require("path");
 const js2xmlparser = require("js2xmlparser");
 
-// Caminhos dos arquivos
+
 const dataDir = path.join(__dirname, "../data");
 const jsonPath = path.join(dataDir, "usuarios.json");
 const xmlPath = path.join(dataDir, "usuarios.xml");
 
-// Garante que a pasta data existe
+
 if (!fs.existsSync(dataDir)) {
   fs.mkdirSync(dataDir, { recursive: true });
 }
 
-// Carrega os usuários do arquivo JSON ao iniciar
+
 let usuarios = [];
 let proximoId = 1;
 if (fs.existsSync(jsonPath)) {
@@ -21,7 +21,7 @@ if (fs.existsSync(jsonPath)) {
     usuarios.length > 0 ? Math.max(...usuarios.map((u) => u.id)) + 1 : 1;
 }
 
-// Função para salvar em JSON e XML
+
 function salvarUsuariosEmArquivo() {
   try {
     fs.writeFileSync(jsonPath, JSON.stringify(usuarios, null, 2), "utf-8");
@@ -32,7 +32,6 @@ function salvarUsuariosEmArquivo() {
   }
 }
 
-// Funções CRUD
 function listarUsuarios() {
   return usuarios;
 }
@@ -41,19 +40,24 @@ function listarUsuarioPeloId(id) {
   return usuarios.find((u) => u.id === id);
 }
 
-function criarUsuario(nome, data_nascimento, email, telefone) {
-  const novoUsuario = { id: proximoId++, nome, data_nascimento, email, telefone};
+function criarUsuario(nome, data_nascimento, email, telefone, cidade, estado, url_imagem) {
+  const novoUsuario = { id: proximoId++, nome, data_nascimento, email, telefone, cidade, estado, url_imagem};
   usuarios.push(novoUsuario);
   salvarUsuariosEmArquivo();
   return novoUsuario;
 }
 
-function atualizarUsuario(id, nome, data_nascimento, email) {
+
+function atualizarUsuario(id, nome, data_nascimento, email, telefone, cidade, estado, url_imagem) {
   const index = usuarios.findIndex((u) => u.id === id);
   if (index === -1) return null;
   if (nome) usuarios[index].nome = nome;
   if (data_nascimento) usuarios[index].data_nascimento = data_nascimento;
   if (email) usuarios[index].email = email;
+  if (telefone) usuarios[index].telefone = telefone;
+  if (cidade) usuarios[index].cidade = cidade;
+  if (estado) usuarios[index].estado = estado;
+  if (url_imagem) usuarios[index].url_imagem = url_imagem;
  
 
   salvarUsuariosEmArquivo();
