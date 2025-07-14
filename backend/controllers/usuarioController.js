f3df4cee11054d1fdd3991725aba70c91bd8b8ba
samreis007 <samtosreis2121@gmail.com>
2025-07-14 15:27:15 -0300

    feat(api): modified "criarUsuario"

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -56,18 +56,20 @@ exports.listarUsuarioPeloId = (req, res) => {
 //================================================================
 
 // Criar um novo usuario
-exports.criarLivro = (req, res) => {
-  let nome, editora, num_paginas, genero, url_capa;
+exports.criarUsuario = (req, res) => {
+  let nome, data_nascimento, email, telefone, cidade, estado, url_imagem;
 
   if (req.is("application/xml")) {
     const dados = req.body.livro;
     nome = dados?.nome?.[0];
-    editora = dados?.editora?.[0];
-    num_paginas = parseInt(dados?.num_paginas?.[0]);
-    genero = dados?.genero?.[0];
-    url_capa = dados?.url_capa?.[0];
+    data_nascimento = dados?.data_nascimento?.[0];
+    email = dados?.email?.[0];
+    telefone = dados?.telefone?.[0];
+    cidade = dados?.cidade?.[0];
+    estado = dados?.estado?.[0];
+    url_imagem = dados?.url_imagem?.[0];
   } else {
-    ({ nome, editora, num_paginas, genero, url_capa } = req.body);
+    ({ nome, data_nascimento, email, telefone, cidade, estado, url_imagem} = req.body);           //nome, data_nascimento, email, telefone, cidade, estado, url_imagem
   }
 
   if (!nome || !editora || !num_paginas || !genero || !url_capa) {
